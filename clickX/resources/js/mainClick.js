b936de393fdb65e036f38f00309633371cfb864e
Paras Bokhari <parasbokhari@gmail.com>
2018-01-15 23:27:55 -0600

    great

--- clickX/resources/js/mainClick.js
+++ clickX/resources/js/mainClick.js
@@ -12,37 +12,37 @@ $(document).ready(function() {
     // return false;
   });
 
-$('#first_section').jarallax({
-    speed: 0.2
-});
+// $('#first_section').jarallax({
+//     speed: 0.2
+// });
 
-$('#third_section').jarallax({
-    speed: 0.2
-});
+// $('#third_section').jarallax({
+//     speed: 0.2
+// });
 
-$('.reel_One').jarallax({
-    speed: 0.2
-});
+// $('.reel_One').jarallax({
+//     speed: 0.2
+// });
 
-$('.reel_Two').jarallax({
-    speed: 0.2
-});
+// $('.reel_Two').jarallax({
+//     speed: 0.2
+// });
 
-$('.reel_Three').jarallax({
-    speed: 0.2
-});
+// $('.reel_Three').jarallax({
+//     speed: 0.2
+// });
 
-$('.reel_Four').jarallax({
-    speed: 0.2
-});
+// $('.reel_Four').jarallax({
+//     speed: 0.2
+// });
 
-$('.reel_Five').jarallax({
-    speed: 0.2
-});
+// $('.reel_Five').jarallax({
+//     speed: 0.2
+// });
 
-$('.reel_Six').jarallax({
-    speed: 0.2
-});
+// $('.reel_Six').jarallax({
+//     speed: 0.2
+// });
 
 
 
